0122893a39bf95236c974fd85606c46767b97e29
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-04-08 22:55:54 +0100

    Add if to end the came in  getNewQuestion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,80 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("answer-select"));
+const choices = Array.from( document.getElementsByClassName("answer-text"));
 
 let currentQuestion = {};
-let acceptingQuestion = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestion = [];
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "What is the primary goal of day trading?",
+        choice1: "Investment",
+        choice2: "Quick profit",
+        choice3: "Retirement",
+        choice4: "Financial security",
+        answer: 1
+    },
+    {
+        question: "= trading?",
+        choice1: "Investment",
+        choice2: "Quick profit",
+        choice3: "Retirement",
+        choice4: "Financial security",
+        answer: 2
+    },
+    {
+        question: "Whary goal of day trading?",
+        choice1: "Investment",
+        choice2: "Quick profit",
+        choice3: "Retirement",
+        choice4: "Financial security",
+        answer: 3
+    },
+] ;
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
-startQuestion = () {
+function startGame() {
     questionCounter = 0;
     score = 0;
-    availableQuestion = [...question];
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
     getNewQuestion();
 }
 
-getNewQuestion = () {
+function getNewQuestion() {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        return window.location.assign("end.html");
+    }
+    
     questionCounter++;
-    const qustionIndex = Math.floor(Math.random() = availableQuestion.length);
-        currentQuestion = availableQuestion[qustionIndex];
-        question.innerText = currentQuestion;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    console.log(availableQuestions);
+    acceptingAnswers = true;
 }
 
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if (!acceptingAnswers) return;
 
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
 
-/* let question = [
-    {
-        question: "What is the primary goal of day trading?",
-        answer1: "Investment",
-        answer2: "Quick profit",
-        answer3: "Retirement",
-        answer4: "Financial security",
-        answer: 1
-    },
-    {
-        question: "What is the primary goal of day trading?",
-        answer1: "Investment",
-        answer2: "Quick profit",
-        answer3: "Retirement",
-        answer4: "Financial security",
-        answer: 2
-    },
-] */
+        getNewQuestion();
+    });
+});
+
+startGame();
