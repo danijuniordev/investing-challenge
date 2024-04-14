7c9a13963eaf7ef9600be2d59e78271bd52efd5b
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-04-14 18:14:03 +0100

    Add save high score to a local storege

--- assets/js/end.js
+++ assets/js/end.js
@@ -4,7 +4,8 @@ const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
 
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-console.log(highScores);
+
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -17,8 +18,13 @@ function saveHighScore(e) {
     e.preventDefault();
 
     const score = {
-        score: mostRecentScore,
+        score: Math.floor(Math.random() * 100),
         name: username.value
     };
-    console.log(score);
+    highScores.push(score);
+    highScores.sort( (a,b) => b.score - a.score);
+    highScores.splice(5);
+
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    window.location.assign("/")
 }
