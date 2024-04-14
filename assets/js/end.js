be8e3b80c0c3da1bcd1fccaf7bb2c6a1fed33149
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-04-14 15:34:52 +0100

    Display final score after you finish the quiz

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,10 +1,15 @@
 const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
+const finalScore = document.getElementById("finalScore");
+
+const mostRecentScore = localStorage.getItem("mostRecentScore");
+finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () =>{
     saveScoreBtn.disabled = !username.value;
 });
 
-saveHighScore = (e) => {
-    
+function saveHighScore(e) {
+    console.log("Clicked the save button");
+    e.preventDefault();
 }
