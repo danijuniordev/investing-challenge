2821a6cfd68f7d202099acedaad07c0bf9a545de
Daniel Pereira Junior <danipereirajr@gmail.com>
2024-04-15 19:54:37 +0100

    Add border to inder page

--- assets/js/end.js
+++ assets/js/end.js
@@ -2,9 +2,7 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
-
 const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
